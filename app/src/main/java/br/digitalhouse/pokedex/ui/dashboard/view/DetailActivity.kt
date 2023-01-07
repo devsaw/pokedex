caa91c221d6f861e15002999e4c89359c2bb6357
Saulo Carvalho <saulocarvalho93@gmail.com>
2023-01-07 17:50:19 -0300

    Implementa shape para cada validação em detalhes

--- app/src/main/java/br/digitalhouse/pokedex/ui/dashboard/view/DetailActivity.kt
+++ app/src/main/java/br/digitalhouse/pokedex/ui/dashboard/view/DetailActivity.kt
@@ -1,9 +1,13 @@
 package br.digitalhouse.pokedex.ui.dashboard.view
 
+import android.animation.ValueAnimator
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.BounceInterpolator
+import android.view.animation.OvershootInterpolator
+import android.widget.Toast
 import br.digitalhouse.pokedex.R
 import br.digitalhouse.pokedex.databinding.ActivityDetailBinding
 import com.bumptech.glide.Glide
@@ -18,13 +22,19 @@ class DetailActivity : AppCompatActivity() {
         setOnClickListener()
     }
 
+    override fun onResume() {
+        super.onResume()
+        animation()
+    }
+
     private fun setOnClickListener() {
         binding.btnBack.setOnClickListener{
             finish()
         }
 
-        binding.fav.setOnClickListener{
+        binding.btnFav.setOnClickListener{
             binding.fav.setImageResource(R.drawable.ic_heartred)
+            Toast.makeText(this, "Adicionado aos favoritos!", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -58,48 +68,63 @@ class DetailActivity : AppCompatActivity() {
         if (type == "Fire"){
             background.setBackgroundColor(Color.parseColor("#FF5C34"))
             binding.type.setImageResource(R.drawable.fire)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_fire)
         }else if(type == "Water"){
             background.setBackgroundColor(Color.parseColor("#30B6FE"))
             binding.type.setImageResource(R.drawable.water)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_water)
         }else if(type == "Grass"){
             background.setBackgroundColor(Color.parseColor("#64DD17"))
             binding.type.setImageResource(R.drawable.grass)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_grass)
         }else if(type == "Electric"){
             background.setBackgroundColor(Color.parseColor("#FFDE08"))
             binding.type.setImageResource(R.drawable.electric)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_electric)
         }else if(type == "Flying"){
             background.setBackgroundColor(Color.parseColor("#F7FF08"))
             binding.type.setImageResource(R.drawable.flying)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_flying)
         }else if(type == "Ice"){
             background.setBackgroundColor(Color.parseColor("#59D8C5"))
             binding.type.setImageResource(R.drawable.ice)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_ice)
         }else if(type == "Psychic"){
             background.setBackgroundColor(Color.parseColor("#ED5484"))
             binding.type.setImageResource(R.drawable.psyc)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_psychic)
         }else if(type == "Rock"){
             background.setBackgroundColor(Color.parseColor("#ECD3AE"))
             binding.type.setImageResource(R.drawable.rock)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_rock)
         }else if(type == "Normal"){
             background.setBackgroundColor(Color.parseColor("#555555"))
             binding.type.setImageResource(R.drawable.normal)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_normal)
         }else if(type == "Poison" || type == "Bug"){
             background.setBackgroundColor(Color.parseColor("#BBFF00"))
             binding.type.setImageResource(R.drawable.poison)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_poison)
         }else if(type == "Fighting"){
             background.setBackgroundColor(Color.parseColor("#913B3B"))
             binding.type.setImageResource(R.drawable.fighting)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_fighting)
         }else if(type == "Ground"){
             background.setBackgroundColor(Color.parseColor("#FFA200"))
             binding.type.setImageResource(R.drawable.ground)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_ground)
         }else if(type == "Ghost"){
             background.setBackgroundColor(Color.parseColor("#E39FFD"))
             binding.type.setImageResource(R.drawable.ghost)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_ghost)
         }else if(type == "Dragon"){
             background.setBackgroundColor(Color.parseColor("#5294B4"))
             binding.type.setImageResource(R.drawable.dragon)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav_dragon)
         }else{
             background.setBackgroundColor(Color.parseColor("#FFFFFFFF"))
             binding.type.setImageResource(R.drawable.ic_pokeballsvg)
+            binding.btnFav.setBackgroundResource(R.drawable.shape_button_fav)
         }
 
         if (weaknesses == "Fire"){
@@ -145,5 +170,19 @@ class DetailActivity : AppCompatActivity() {
         }else{
             binding.nextEvo.text = nextEvo
         }
+    }
+
+    private fun animation(){
+        val animator= ValueAnimator.ofFloat(0f, 1f)
+        animator.duration = 3000
+        animator.interpolator = BounceInterpolator()
+        animator.addUpdateListener { valueAnimator ->
+            val animatedValue = valueAnimator.animatedValue as Float
+            binding.background.alpha = animatedValue
+            binding.foto.alpha = animatedValue
+            binding.foto.scaleX = animatedValue
+            binding.foto.scaleY = animatedValue
+        }
+        animator.start()
     }
 }
